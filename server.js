e590ae28d2573bb68f51e579297eb522e03b0214
Bryan Taboada <b.taboada123@gmail.com>
2024-07-30 14:18:14 -0600

    viewAllEmployees is now rendering the data correctly

--- server.js
+++ server.js
@@ -122,15 +122,32 @@ function viewAllRoles() {
 }
 
 function viewAllEmployees() {
-    pool.query('SELECT * FROM employee;', (err, res) => {
-        if (err) {
-            console.error('Error fetching departments:', err);
-        } else {
-            console.log('');
-            console.table(res.rows);
-            promptUser();
-        }
-    });
+    pool.query(
+`SELECT 
+    e.id, 
+    e.first_name, 
+    e.last_name, 
+    r.title, 
+    d.name AS department, 
+    r.salary, 
+    CONCAT(m.first_name, ' ', m.last_name) AS manager
+FROM 
+    employee e
+LEFT JOIN 
+    role r ON e.role_id = r.id
+LEFT JOIN 
+    department d ON r.department_id = d.id
+LEFT JOIN 
+    employee m ON e.manager_id = m.id;`,
+        (err, res) => {
+            if (err) {
+                console.error('Error fetching departments:', err);
+            } else {
+                console.log('');
+                console.table(res.rows);
+                promptUser();
+            }
+        });
 
 
 }
